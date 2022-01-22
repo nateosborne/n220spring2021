137c3f73f37b2deba69f051fc1b0c5d4274318b7
nateosborne <natedresdeno@gmail.com>
2022-01-22 16:52:07 -0500

    changed home page

--- n220LabsSP2022/Labs/Lab1/js/app.js
+++ n220LabsSP2022/Labs/Lab1/js/app.js
@@ -1,7 +1,18 @@
+let size = 10;
+
 function setup() {
+
     createCanvas(800, 600);
+
+    circle(150,50,30);
+    circle(300,50,30);
+
+    arc(200,200,200,150,150,0 PI + QUARTER_PI, CHORD);
+
 }
 
-function draw() {
-    circle(mouseX, mouseY, frameCount/10);
+function draw(){
+    fill(255,99,99);
+    circle(150,150,size);
+    size = size + 1;
 }
